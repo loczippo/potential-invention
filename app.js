a9c84f1f204e6bbbf0b177f08cb057edd7e9c0c9
aphloc <kimloc1000@gmail.com>
2023-05-23 22:07:03 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const { google } = require('googleapis')
+
+const GOOGLE_API_FOLDER_ID = '1hTqUjEEwzQYUi5Bsl1PJ3SggH9OTgdFi'
+
+async function uploadFile(){
+    try{
+        const auth = new google.auth.GoogleAuth({
+            keyFile: './googlekey.json',
+            scopes: ['https://www.googleapis.com/auth/drive']
+        })
+
+        const driveService = google.drive({
+            version: 'v3',
+            auth
+        })
+
+        const fileMetaData = {
+            'name': '1.jpg',
+            'parents': [GOOGLE_API_FOLDER_ID]
+        }
+
+        const media = {
+            mimeType: 'image/jpg',
+            body: fs.createReadStream('./1.jpg')
+        }
+
+        const response = await driveService.files.create({
+            resource: fileMetaData,
+            media: media,
+            field: 'id'
+        })
+        return response.data.id
+
+    }catch(err){
+        console.log('Upload file error', err)
+    }
+}
+
+uploadFile().then(data => {
+    console.log(data)
+    //https://drive.google.com/uc?export=view&id=
+})
